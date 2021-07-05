2eba3ee3f7d8e67322ce3e31d0af520b4be8c5d3
Deustavo <gustavo.andrade00@outlook.com>
2021-07-05 10:55:09 -0300

    firsth commmit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-page">
+    <div class="container-header">
+      <img src="@/assets/trayLogo.png" alt="logo tay">
+      <h1 class="title-page">Lista de compras</h1>
+      <form @submit.prevent="addTodo(todo)">
+        <div class="container-input">
+          <input type="text" v-model="todo.description" class="form-input" placeholder="Add todo" />
+          <button class="form-button">Adicionar</button>
+        </div>
+      </form>
+    </div>
+    <div class="container-todo-list">
+      <todo
+        v-for="t in todos"
+        :key="t.id"
+        :todo="t"
+        @toggle="toggleTodo"
+        @remove="removeTodo"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { Todo },
+  data () {
+    return { todos: [], todo: { checked: false } }
+  },
+  methods: {
+    addTodo (todo) {
+      todo.id = Date.now()
+      this.todos.push(todo)
+      this.todo = { checked: false }
+    },
+    toggleTodo (todo) {
+      const index = this.todos.findIndex(item => item.id === todo.id)
+      if (index > -1) {
+        const checked = !this.todos[index].checked
+        // $set será usado para alterar o valor da array de forma segura, o primeiro parametro é a lista, o segundo o index do item na lista e o treceiro valor o que deseja atribuir
+        this.$set(this.todos, index, { ...this.todos[index], checked })
+      }
+    },
+    removeTodo (todo) {
+      const index = this.todos.findIndex(item => item.id === todo.id)
+      if (index > -1) {
+        this.$delete(this.todos, index)
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
+  .container-page {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    font-family: 'Raleway', sans-serif;
+  }
+  .container-header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: 40px;
+  }
+  .container-header img {
+    max-width: 200px;
+  }
+  .container-input {
+    display: flex;
+    align-items: center;
+    width: 432px;
+  }
+  .form-input {
+    width: 100%;
+    padding: 8px;
+  }
+  .form-button {
+    padding: 10px;
+    background-color: #5c5ce0;
+    border-radius: 4px;
+    border: none;
+    cursor: pointer;
+  }
+  .title-page {
+    font-size: 56px;
+    margin: 0px 0px 40px 0px;
+  }
+  .container-todo-list {
+    margin: 8px 0px 40px 0px;
+    max-height: calc(100vh);
+    overflow: auto;
+  }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="todo-item" :class="{checked: todo.checked}">
+    <span class="todo-arrow" style="margin: 0px 12px 0px 0px">&#8594;</span>
+    <p class="todo-title">{{ todo.description }}</p>
+    <div class="todo-options">
+      <button
+        class="todo-button"
+        style="background-color: #5ce05c"
+        @click="$emit('toggle', todo)"
+      >
+        <span v-if="todo.checked">Desmarcar</span>
+        <span v-else>Confere</span>
+      </button>
+      <button
+        class="todo-button"
+        style="background-color: #ff5959"
+        @click="$emit('remove', todo)"
+      >
+        Remover
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: { type: Object, required: true }
+  }
+}
+</script>
+
+<style>
+  .todo-item {
+    background-color: #f1f1f1;
+    color: black;
+    display: flex;
+    align-items: center;
+    padding: 0px 16px;
+    margin: 8px 0px;
+    width: 400px;
+    border-radius: 8px;
+  }
+  .todo-title {
+    width: 240px;
+    word-break: break-all;
+    margin-right: 16px;
+  }
+  .todo-options {
+    width: min-content;
+    margin: 12px 0px;
+  }
+  .todo-button {
+    width: 100px;
+    padding: 8px;
+    cursor: pointer;
+    border-radius: 4px;
+    border: none;
+    margin: 8px 0px;
+  }
+  .checked {
+    background-color: #e7ffe7;
+  }
+  .checked .todo-title, .checked .todo-arrow {
+    color: #d0d0d0;
+  }
+</style>
